ab6636042df462674e253668cd42b9ff2d8d711f
Ara <ara.akopyan@gmail.com>
2021-12-03 00:07:45 +0400

    feat: try to imagine a real situation with calculator of 3rd triangle and look at it from user side

--- Third_Angle_Triangle.js
+++ Third_Angle_Triangle.js
@@ -2,3 +2,18 @@
 let angle1=prompt("First angle?","");
 let angle2=prompt("Second angle?","");
 console.log(180-angle1-angle2);
+
+
+let angle1 = prompt("First angle is...","");
+if(angle1>180||angle1 <0){
+  alert("Angle of triangle can't be more 180 or less 0!");
+  let angle1=prompt("Try again, first angle is...","");
+} else {
+let angle2 = prompt("Second angle is ...","");
+}
+let angle3 = 180 - angle1 - angle2
+if (angle3=0) {
+    console.log("Third Angle of triangle equals 0, so it's a line");
+} else {
+    console.log(angle3);
+}

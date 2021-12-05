9ae1d188a550459d60e6d8b53239abb2f6ae7bff
Ara <ara.akopyan@gmail.com>
2021-12-06 00:25:17 +0400

    feat: create html version of triangle task

--- Third_Angle_Triangle.js
+++ Third_Angle_Triangle.js
@@ -1,19 +1,21 @@
-"use strict"
-let angle1=prompt("First angle?","");
-let angle2=prompt("Second angle?","");
-console.log(180-angle1-angle2);
-
-
 let angle1 = prompt("First angle is...","");
 if(angle1>180||angle1 <0){
   alert("Angle of triangle can't be more 180 or less 0!");
-  let angle1=prompt("Try again, first angle is...","");
+  angle1=prompt("Try again, first angle is...","");
 } else {
-let angle2 = prompt("Second angle is ...","");
+
 }
-let angle3 = 180 - angle1 - angle2
-if (angle3=0) {
-    console.log("Third Angle of triangle equals 0, so it's a line");
+let angle2 = prompt("Second angle is ...","");
+if(angle2>180-angle1||angle2 <0){
+  alert("Angle of triangle can't be more 180 or less 0!");
+  angle2=prompt("Try again, second angle is...","");
 } else {
-    console.log(angle3);
+
+}
+const sum= 180
+let angle3 = sum - angle1 - angle2
+if (angle3==0) {
+    alert("Third Angle of triangle equals 0, so it's a line");
+} else {
+    alert("Third angle is: "+angle3);
 }
